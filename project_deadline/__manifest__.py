c805279ae06a7ee2456594a6832a1e2bda3e6f42
alielgarhi <el_garhi@hotmail.com>
2019-12-24 10:35:07 +0200

    ejad

---
+++ project_deadline/__manifest__.py
@@ -0,0 +1,19 @@
+# Copyright 2019 Onestein
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+
+{
+    'name': "Project Deadline",
+    'summary': 'Start date and deadline of projects.',
+    'author': 'Ali Elgarhi - NDS4IT',
+    'license': 'AGPL-3',
+    'website': 'NDS4it.com',
+    'category': 'Project Management',
+    'version': '13.0.1.0.0',
+    'depends': [
+        'project'
+    ],
+    'data': [
+        'views/project_project_view.xml',
+        'views/project_task_view.xml'
+    ]
+}
